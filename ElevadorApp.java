3dce0935f97dfa52276a7392eaee338337aff65b
TheuZCoder <matheusrdsilva1@gmail.com>
2024-01-29 17:03:09 -0300

    atualizações visuais

--- ElevadorApp.java
+++ ElevadorApp.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 public class ElevadorApp extends JFrame {
 
@@ -12,7 +14,9 @@ public class ElevadorApp extends JFrame {
     private JButton[] botoesElevador;
     private JTextField andarDestinoTextField;
     private JPanel[] luzesElevador;
-    private JPanel[] andarIndicators; // Barra vertical para indicar a posição do elevador em cada andar
+    private JPanel[] andarIndicators;
+    private JTextArea consoleTextArea; // Componente de texto para exibir a saída do console
+    private PrintStream printStream; // Stream para redirecionar a saída do console
 
     private int[] andarAtualElevador;
     private boolean[] emMovimento;
@@ -24,7 +28,6 @@ public class ElevadorApp extends JFrame {
         setLayout(new BorderLayout());
 
         try {
-            // Define o Look and Feel Nimbus para uma aparência mais moderna
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
             SwingUtilities.updateComponentTreeUI(this);
         } catch (Exception e) {
@@ -57,8 +60,8 @@ public class ElevadorApp extends JFrame {
             luzesElevador[i].setPreferredSize(new Dimension(20, 20));
             luzesElevador[i].setBackground(Color.RED);
             andarIndicators[i] = new JPanel();
-            andarIndicators[i].setPreferredSize(new Dimension(20, 200)); // Tamanho da barra vertical
-            andarIndicators[i].setBackground(Color.GRAY); // Cor da barra vertical (pode ser ajustada)
+            andarIndicators[i].setPreferredSize(new Dimension(20, 200));
+            andarIndicators[i].setBackground(Color.GRAY);
             final int elevador = i;
             botoesElevador[i].addActionListener(new ActionListener() {
                 @Override
@@ -70,6 +73,16 @@ public class ElevadorApp extends JFrame {
 
         // Inicializa o campo de texto para o andar de destino
         andarDestinoTextField = new JTextField();
+
+        // Inicializa o componente de texto para a saída do console
+        consoleTextArea = new JTextArea();
+        consoleTextArea.setEditable(false); // Impede que o usuário edite o texto
+        JScrollPane scrollPane = new JScrollPane(consoleTextArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        // Redireciona a saída do console para o componente de texto
+        printStream = new PrintStream(new CustomOutputStream(consoleTextArea));
+        System.setOut(printStream);
 
         // Adiciona os componentes à janela
         JPanel painelAndares = new JPanel(new GridLayout(NUM_ANDARES, 1));
@@ -92,8 +105,9 @@ public class ElevadorApp extends JFrame {
         painelControles.add(painelBotoesElevador, BorderLayout.EAST);
         painelControles.add(painelLuzesElevador, BorderLayout.SOUTH);
 
-        add(painelControles, BorderLayout.NORTH); // Movido para o norte para deixar espaço para as barras verticais
-        add(painelAndarIndicators, BorderLayout.CENTER); // Adiciona as barras verticais ao centro
+        add(painelControles, BorderLayout.NORTH);
+        add(painelAndarIndicators, BorderLayout.CENTER);
+        add(scrollPane, BorderLayout.SOUTH); // Adiciona a caixa de texto para a saída do console
 
         // Ajusta o tamanho da janela automaticamente
         pack();
@@ -107,23 +121,16 @@ public class ElevadorApp extends JFrame {
 
     private void chamarElevador(int andar) {
         int elevadorMaisProximo = encontrarElevadorMaisProximo(andar);
-        System.out.println("Chamando elevador para o Andar " + andar);
+        consoleTextArea.append("Chamando elevador para o Andar " + andar + "\n");
 
         if (emMovimento[elevadorMaisProximo]) {
-            // Se o elevador estiver em movimento, ignore o chamado
-            System.out.println("Elevador " + (elevadorMaisProximo + 1) + " está em movimento.");
+            consoleTextArea.append("Elevador " + (elevadorMaisProximo + 1) + " está em movimento.\n");
         } else {
-            // Atualiza o andar atual do elevador e exibe mensagem
-            System.out.println("Elevador " + (elevadorMaisProximo + 1) + " a caminho do Andar " + andar);
+            consoleTextArea.append("Elevador " + (elevadorMaisProximo + 1) + " a caminho do Andar " + andar + "\n");
             emMovimento[elevadorMaisProximo] = true;
-
-            // Atualiza a luz indicativa para verde (indicando movimento)
             luzesElevador[elevadorMaisProximo].setBackground(Color.GREEN);
-
-            // Atualiza a posição da barra vertical para indicar a posição do elevador
             andarIndicators[elevadorMaisProximo].setPreferredSize(new Dimension(20, (andar + 1) * 20));
 
-            // Simula o movimento do elevador (atualize conforme necessário)
             new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -131,22 +138,18 @@ public class ElevadorApp extends JFrame {
                         int andarAtual = andarAtualElevador[elevadorMaisProximo];
                         int destino = andar;
 
-                        System.out.println("Elevador " + (elevadorMaisProximo + 1) + " em movimento do Andar " +
-                                andarAtual + " para o Andar " + destino);
+                        consoleTextArea.append("Elevador " + (elevadorMaisProximo + 1) +
+                                " em movimento do Andar " + andarAtual + " para o Andar " + destino + "\n");
 
-                        // Simula o tempo que leva para o elevador se mover
                         Thread.sleep(Math.abs(destino - andarAtual) * 1000);
 
-                        andarAtualElevador[elevadorMaisProximo] = destino; // Atualiza o andar atual após o movimento
+                        andarAtualElevador[elevadorMaisProximo] = destino;
                         emMovimento[elevadorMaisProximo] = false;
-
-                        // Atualiza a luz indicativa para vermelho (indicando que o elevador está parado)
                         luzesElevador[elevadorMaisProximo].setBackground(Color.RED);
-
-                        // Reseta a posição da barra vertical para indicar que o elevador parou
                         andarIndicators[elevadorMaisProximo].setPreferredSize(new Dimension(20, (destino + 1) * 20));
 
-                        System.out.println("Elevador " + (elevadorMaisProximo + 1) + " chegou ao Andar " + destino);
+                        consoleTextArea.append("Elevador " + (elevadorMaisProximo + 1) +
+                                " chegou ao Andar " + destino + "\n");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -156,7 +159,6 @@ public class ElevadorApp extends JFrame {
     }
 
     private int encontrarElevadorMaisProximo(int andar) {
-        // Lógica para encontrar o elevador mais próximo
         int elevadorMaisProximo = 0;
         int distanciaMinima = Math.abs(andar - andarAtualElevador[0]);
 
@@ -173,47 +175,52 @@ public class ElevadorApp extends JFrame {
 
     private void definirAndarDestino(int elevador) {
         if (emMovimento[elevador]) {
-            // Se o elevador estiver em movimento, ignore a solicitação
-            System.out.println("Elevador " + (elevador + 1) + " está em movimento.");
+            consoleTextArea.append("Elevador " + (elevador + 1) + " está em movimento.\n");
         } else {
             String andarDestinoStr = andarDestinoTextField.getText();
 
             try {
                 int andarDestino = Integer.parseInt(andarDestinoStr);
-                // Atualiza o andar de destino do elevador e exibe mensagem
-                System.out.println("Elevador " + (elevador + 1) + " indo para o Andar " + andarDestino);
+                consoleTextArea.append("Elevador " + (elevador + 1) + " indo para o Andar " + andarDestino + "\n");
                 emMovimento[elevador] = true;
-
-                // Atualiza a luz indicativa para verde (indicando movimento)
                 luzesElevador[elevador].setBackground(Color.GREEN);
-
-                // Atualiza a posição da barra vertical para indicar a posição do elevador
                 andarIndicators[elevador].setPreferredSize(new Dimension(20, (andarDestino + 1) * 20));
 
-                // Simula o movimento do elevador (atualize conforme necessário)
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            Thread.sleep(3000); // Simula o tempo que leva para o elevador se mover
+                            Thread.sleep(3000);
                             andarAtualElevador[elevador] = andarDestino;
                             emMovimento[elevador] = false;
-
-                            // Atualiza a luz indicativa para vermelho (indicando que o elevador está parado)
                             luzesElevador[elevador].setBackground(Color.RED);
-
-                            // Reseta a posição da barra vertical para indicar que o elevador parou
                             andarIndicators[elevador].setPreferredSize(new Dimension(20, (andarDestino + 1) * 20));
 
-                            System.out.println("Elevador " + (elevador + 1) + " chegou ao Andar " + andarDestino);
+                            consoleTextArea.append("Elevador " + (elevador + 1) +
+                                    " chegou ao Andar " + andarDestino + "\n");
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
                     }
                 }).start();
             } catch (NumberFormatException e) {
-                System.out.println("Informe um número válido para o andar de destino.");
+                consoleTextArea.append("Informe um número válido para o andar de destino.\n");
             }
+        }
+    }
+
+    // Classe para redirecionar a saída do console para o JTextArea
+    private class CustomOutputStream extends OutputStream {
+        private JTextArea textArea;
+
+        public CustomOutputStream(JTextArea textArea) {
+            this.textArea = textArea;
+        }
+
+        @Override
+        public void write(int b) {
+            textArea.append(String.valueOf((char) b));
+            textArea.setCaretPosition(textArea.getDocument().getLength());
         }
     }
 
